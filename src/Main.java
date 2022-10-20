123dc4a6b0222476bc1ce3ae7a0cf756f4899491
Tolga Turhan <tolgaturhan@live.com>
2022-10-20 18:05:37 +0300

    v0.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        float vFloat = 1;
+        System.out.println(vFloat);
+
+        float piSayisi = 3.14f;
+        System.out.println(piSayisi);
 
     }
 }
